4e5192138c079f78fa9c337d0a7428d0de1097d5
niadulMunna <niadulislammunna@gmail.com>
2025-02-25 20:45:09 +0600

    refactor: added functionality

--- src/components/person-one.js
+++ src/components/person-one.js
@@ -1,14 +1,31 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 
 const PersonOne = () => {
+  const [data, setData] = useState(10);
+  const handleIncrease = () => {
+    setData(data + 10);
+  };
+  const handleDecrease = () => {
+    setData(data - 5);
+  };
   return (
     <div className="flex gap-2 items-center">
       <h1>
         John is offering{" "}
-        <span className="font-semibold italic text-lg"> $10</span>
+        <span className="font-semibold italic text-lg"> ${data}</span>
       </h1>
-      <button className="bg-green-500 px-3 py-1  rounded-tr-lg rounded-bl-lg hover:bg-green-600 hover:translate-y-[2px] transition-all ">
+      <button
+        onClick={handleIncrease}
+        className="bg-green-500 px-3 py-1  rounded-tr-lg rounded-bl-lg hover:bg-green-600 hover:translate-y-[2px] transition-all "
+      >
         Increase Money
+      </button>
+      <button
+        onClick={handleDecrease}
+        className="bg-red-500 px-3 py-1 rounded-tr-lg rounded-bl-lg hover:bg-red-600 hover:translate-y-[2px] transition-all "
+      >
+        Decrease Money
       </button>
     </div>
   );
