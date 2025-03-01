05e58c6182c0ed0a0acebda7c796a0499d06e3a9
niadulMunna <niadulislammunna@gmail.com>
2025-03-01 23:26:55 +0600

    style

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import PersonTwo from "@/components/person-two";
 export default function Home() {
   return (
     <div className="flex flex-col items-center  pt-10  w-screen h-screen">
-      <div className="border space-y-5 bg-neutral-800 rounded-tl-xl rounded-br-xl border-black p-4">
+      <div className="border space-y-5 bg-neutral-800 rounded-tr-xl rounded-bl-xl border-black p-4">
         <PersonOne />
         <PersonTwo />
       </div>
